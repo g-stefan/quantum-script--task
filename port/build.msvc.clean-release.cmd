94c923074792e351fc33ca884d0747727d853d2d
Grigore Stefan <g_stefan@yahoo.com>
2020-12-04 13:57:11 +0200

    update

--- port/build.msvc.clean-release.cmd
+++ port/build.msvc.clean-release.cmd
@@ -3,6 +3,6 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
-echo -^> clean-local-release quantum-script-extension-task
+echo -^> clean-release quantum-script-extension-task
 
 if exist release\ rmdir /Q /S release
