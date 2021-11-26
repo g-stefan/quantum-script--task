ec47027fa64a141ef376f89aebf4bcf061b20f47
Grigore Stefan <g_stefan@yahoo.com>
2021-11-26 03:23:36 +0200

    Update

--- build/msvc.make.cmd
+++ build/msvc.make.cmd
@@ -6,9 +6,9 @@ rem Created by Grigore Stefan <g_stefan@yahoo.com>
 set ACTION=%1
 if "%1" == "" set ACTION=make
 
-echo -^> %ACTION% quantum-script-extension-task
+echo - %BUILD_PROJECT% ^> %1
 
-goto StepX
+goto cmdXDefined
 :cmdX
 %*
 if errorlevel 1 goto cmdXError
@@ -16,7 +16,7 @@ goto :eof
 :cmdXError
 echo "Error: %ACTION%"
 exit 1
-:StepX
+:cmdXDefined
 
 call :cmdX file-to-cs --touch=source/quantum-script-extension-task.cpp --file-in=source/quantum-script-extension-task.js --file-out=source/quantum-script-extension-task.src --is-string --name=extensionTaskSource
 call :cmdX xyo-cc --mode=%ACTION% @build/source/quantum-script-extension-task.static.compile
